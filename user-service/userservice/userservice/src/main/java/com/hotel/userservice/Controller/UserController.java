2330a5e7191c8e3d575f8702edf61ac8e82041de
Wethmal <kisanduofficially@gmail.com>
2025-06-28 13:49:32 +0530

    kisandu_user
    created...backend

---
+++ user-service/userservice/userservice/src/main/java/com/hotel/userservice/Controller/UserController.java
@@ -0,0 +1,43 @@
+package com.hotel.userservice.Controller;
+
+import com.hotel.userservice.Model.User;
+import com.hotel.userservice.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) {
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
---
+++ user-service/userservice/userservice/src/main/java/com/hotel/userservice/Service/UserService.java
@@ -0,0 +1,34 @@
+package com.hotel.userservice.Service;
+
+import com.hotel.userservice.Model.User;
+import com.hotel.userservice.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+}
